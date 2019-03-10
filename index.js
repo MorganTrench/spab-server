23e66584d5d819acbbae5e31b33c96e19e8461a5
MorganTrench <morgan.trench@gmail.com>
2019-03-10 21:33:43 +0800

    Externalised Config

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const path = require('path');
 /* Web framework */
 const express = require('express');
 const bodyParser = require('body-parser');
+/* Externalised Config */
+const dotenv = require('dotenv');
+dotenv.config();
 /* Our modules */
 const api = require('./api'); // api endpoints
 const static = require('./static'); // serve static files
@@ -10,7 +13,7 @@ const db = require('./models'); // initialise the database
 
 /* Setup express */
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Setup api endpoing, parse body of incoming requests into Javascript objects
 app.use('/api', bodyParser.json(), bodyParser.urlencoded(), api);
