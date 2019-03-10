c3eae5dbb04872e944648a269b05579954b17569
MorganTrench <morgan.trench@gmail.com>
2019-03-10 14:16:19 +0800

    Setup for serving frontend with express

--- static/index.js
+++ static/index.js
@@ -8,6 +8,12 @@ const photoDirectoryPath = path.join(__dirname, 'photos');
 router.use('/photos', express.static(photoDirectoryPath));
 
 // Frontend
-// TODO
+const frontendDirectoryPath = path.join(__dirname, 'frontend');
+// Have app routes redirected to index.html so that internal routes work
+router.use(['/journey', '/plan', '/sensor', '/status'], (req, res) => {
+  res.sendFile(frontendDirectoryPath + '/index.html');
+});
+// Serve al frontend files
+router.use('/', express.static(frontendDirectoryPath, options));
 
 module.exports = { photoDirectoryPath, router };
